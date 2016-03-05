90e09a0a13e35d5ff9f3489e2aae86ef64ca820a
wronfim <wronfim@gmail.com>
2016-03-05 16:19:33 -0500

    Split html into react class methods

--- client/admin/Admin.jsx
+++ client/admin/Admin.jsx
@@ -13,56 +13,59 @@ Admin = React.createClass({
       brands: Brands.find({}).fetch()
     }
   },
+  categoriesHeader() {
+    return (
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th></th>
+      </tr>
+    );
+  },
+  categoriesBody(){
+    return this.data.categories.map((category) =>{
+        return (
+          <tr>
+            <td>{ category.name }</td>
+            <td>{ category.description }</td>
+            <td className="single line">
+              <span><i className="large unhide icon"></i></span>
+              <span><i className="large edit icon"></i></span>
+              <span><i className="large remove icon"></i></span>
+            </td>
+          </tr>
+        );
+    });
+  },
+  menuItems() {
+    var items = [
+      { name: 'Products', icon: 'shop icon', size: this.data.products.length, class: "active item" },
+      { name: 'Categories', icon: 'unordered list icon', size: this.data.categories.length, class: "item" },
+      { name: 'Manufacturers', icon: 'configure icon', size: this.data.manufacturers.length, class: "item" },
+      { name: 'Brands', icon: 'barcode icon', size: this.data.brands.length, class: "item" },
+      { name: 'Promotions', icon: 'announcement icon', size: 0, class: "item" },
+      { name: 'Videos', icon: 'film icon', size: 0, class: "item" }
+    ];
+    return (
+      <div className="ui vertical pointing menu">
+        { items.map((item) => {
+            return (
+              <a className={ item.class }>
+                <span><i className={ item.icon } ></i></span>
+                { item.name }
+                <div className="ui label"> { item.size } </div>
+              </a>
+            );
+          })
+        }
+      </div>
+    );
+  },
   render(){
-    var iconSize = {
-      fontSize: '9em',
-      marginTop: '50px',
-      marginBottom: '0'
-    };
-
     return(
       <div className="ui grid">
         <div className="four wide column">
-          <div className="ui vertical pointing menu">
-            <a className="active item">
-              <span><i className="shop icon"></i></span>
-              Products
-              <div className="ui label">
-                { this.data.products.length }
-              </div>
-            </a>
-            <a className="item">
-              <span><i className="unordered list icon"></i></span>
-              Categories
-              <div className="ui label">
-                { this.data.categories.length }
-              </div>
-            </a>
-            <a className="item">
-              <span><i className="configure icon"></i></span>
-              Manufactures
-              <div className="ui label">
-                { this.data.manufacturers.length }
-              </div>
-            </a>
-            <a className="item">
-              <span><i className="barcode icon"></i></span>
-              Brands
-              <div className="ui label">
-                { this.data.brands.length }
-              </div>
-            </a>
-            <a className="item">
-              <span><i className="announcement icon"></i></span>
-              Promotions
-              <div className="ui label">0</div>
-            </a>
-            <a className="item">
-              <span><i className="film icon"></i></span>
-              Videos
-              <div className="ui label">0</div>
-            </a>
-          </div>
+          { this.menuItems() }
         </div>
         <div className="twelve wide stretched column">
           <div className="ui segment">
@@ -72,37 +75,15 @@ Admin = React.createClass({
                 <i className="plus icon"></i>
               </div>
             </div>
-
             <table className="ui compact celled table">
               <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Description</th>
-                  <th>Weight</th>
-                  <th>Size</th>
-                  <th>Price</th>
-                  <th>In Stock</th>
-                  <th>Brand</th>
-                  <th>Category</th>
-                  <th></th>
-                </tr>
+                { this.categoriesHeader() }
               </thead>
               <tbody>
-                <td>Dummy Product</td>
-                <td>Creatine is the reference compound for power improvement, with numbers from one meta-analysis to assess potency</td>
-                <td>123</td>
-                <td className="single line">12 x 12 x 12</td>
-                <td>$12.00</td>
-                <td>454</td>
-                <td>Fake Company</td>
-                <td>Falsified</td>
-                <td className="single line">
-                  <span><i className="large unhide icon"></i></span>
-                  <span><i className="large edit icon"></i></span>
-                  <span><i className="large remove icon"></i></span>
-                </td>
+                { this.categoriesBody() }
               </tbody>
             </table>
+
           </div>
         </div>
       </div>
